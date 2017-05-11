0c6326fc2187e79b2c78a806366e2c4ba52d1545
Alexis Duran <alexis@hashlabs.com>
2017-05-11 09:39:34 -0400

    Add working example using ThemeProvider sample code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const theme = (ComponentToWrap) => {
+  return class ThemeComponent extends Component {
+    // let’s define what’s needed from the `context`
+    static contextTypes = {
+      theme: PropTypes.object.isRequired,
+    }
+    render() {
+      const { theme } = this.context
+      // what we do is basically rendering `ComponentToWrap`
+      // with an added `theme` prop, like a hook
+      return (
+        <ComponentToWrap {...this.props} theme={theme} />
+      )
+    }
+  }
+}
+
+
 class App extends Component {
   render() {
+    const { theme } = this.props;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 style={{ color: theme.color }}>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +38,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default theme(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component, PropTypes, Children } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+class ThemeProvider extends Component {
+  static propTypes = {
+    theme: PropTypes.object.isRequired,
+  }
+  // you must specify what you’re adding to the context
+  static childContextTypes = {
+    theme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+   const { theme } = this.props
+   return { theme }
+  }
+
+  render() {
+    // `Children.only` enables us not to add a <div /> for nothing
+    return Children.only(this.props.children)
+  }
+}
+
+const theme = {
+  color: "#cc3300",
+  fontFamily: "Georgia",
+};
+
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
